cab4b0e9ce9d8c63f3b165564c0a2b3f6ab00d0a
Jonas Andrée <jonandre@kth.se>
2012-11-25 00:35:51 +0100

    Rotation and gas is fixed
    To do list:
    • Connect server with Graphic Rendering client
    • Write on the screen the IP number of the server.
    • Make it possible with a pop up on the mobile to enter the IP number of
    the server
    • Make it possible with a pop up on the Graphic Rendering client to
    enter a IP for the server
    • Sound in the mobile phone
    • Sound on the Graphics Screen
    • Make Hit detection
    • Add Shots
    • Make it possible on the mobile to add a name
    • Write the name on the Graphics Screen
    • Volumetric clouds
    • Make a better gas and rotation system. The gas needs to go up or down.
    Not from one to hundred and you can’t rotate the airplane while not
    using the gas.

--- server/ADGI P2 Server/src/se/jonasandree/airbattles/server/Client.java
+++ server/ADGI P2 Server/src/se/jonasandree/airbattles/server/Client.java
@@ -13,48 +13,71 @@ public class Client {
 		this.scanner = new Scanner(this.in);
 	}
 	
+	public Airplane airplane;
+	
+	public int screenWidth;
+	public int screenHeight;
+	
 	public final int index;
 	public final InputStream in;
 	public final OutputStream out;
 	public final Scanner scanner;
+	public boolean android = false;
 	
-	public void start() throws IOException
-	{
+	public void start() throws IOException{
 		for (String line; this.scanner.hasNextLine();){
 			line = this.scanner.nextLine();
 			if (line.isEmpty()){
-				System.out.print(".");
+				System.out.print("");
 				System.out.flush();
 			}
-			else{				
-				if (line.substring(0, 7).equals("Android")){
-					System.out.print("\n" + line + " index "+ this.index);
-				}
-				else if (line.substring(0, 6).equals("screen")){
-					if(line.substring(6, 7).equals("W")){
-						System.out.print("\n" + line.substring(11) + " index "+ this.index);
-					}
-					if(line.substring(6, 7).equals("H")){
-						System.out.print("\n" + line.substring(12) + " index "+ this.index);
+			else{
+				if (!this.android){
+					if (line.substring(0, 7).equals("Android")){
+						this.airplane = new Airplane(index);
+						android = true;
 					}
 				}
-				else if(line.substring(0, 1).equals("x")){
-					System.out.print("\n" + line.substring(1) + " index "+ this.index);
-				}
-				else if(line.substring(0,1).equals("y")){
-					System.out.print("\n" + line.substring(1) + " index "+ this.index);
-				}
-				else if (line.substring(0, 4).equals("fire")){
-					System.out.print("\n" + line + " index "+ this.index);
-				}
-				else if (line.substring(0, 4).equals("+gas")){
-					System.out.print("\n" + line + " index "+ this.index);
-				}
-				else if (line.substring(0, 4).equals("-gas")){
-					System.out.print("\n" + line + " index "+ this.index);
-				}
-				else{
-					System.out.print("\n Not rekogniceble " + line + " index "+ this.index);
+				else if(this.android){
+					if(line.substring(0, 1).equals("x")){
+						this.airplane.setRotationX(Float.valueOf(line.substring(1)));
+						//System.out.println(line);
+					}
+					else if(line.substring(0, 1).equals("y")){
+						this.airplane.setRotationY(Float.valueOf(line.substring(1)));
+						//System.out.println(this.airplane.getRotation());
+
+						System.out.println("\n speed" + this.airplane.getSpeed());
+						System.out.println("\n rotation "+ this.airplane.getRotation());
+						System.out.println("\n x "+ this.airplane.getPositionX());
+						System.out.println("\n y "+ this.airplane.getPositionY()+ "\n");
+					}
+					else if (line.substring(0, 1).equals("f")){
+						System.out.print("\n" + line + " index "+ this.index);
+					}
+					else if (line.substring(1, 2).equals("g")){
+						if (line.substring(0, 1).equals("+")){
+							float massage = 1-Float.valueOf(line.substring(4))/this.screenHeight;
+							this.airplane.setSpeed(massage);
+						}
+						if (line.substring(0, 1).equals("-")){
+							this.airplane.setSpeed(0);
+							System.out.print("\n" + line + " index "+ this.index);
+						}
+					}
+					else if (line.substring(0, 6).equals("screen")){
+						System.out.println(line);
+						if(line.substring(6, 7).equals("W")){
+							screenWidth = Integer.valueOf(line.substring(11));
+						}
+						if(line.substring(6, 7).equals("H")){
+							screenHeight = Integer.valueOf(line.substring(12));
+						}
+					}
+					else{
+						System.out.print("\n Not rekogniceble " + line + " index "+ this.index);
+						System.out.flush();
+					}
 				}
 			}
 		}
