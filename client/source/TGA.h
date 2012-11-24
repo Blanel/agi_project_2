5da898715f627a6edcf326bbe9f030a366908713
Erik N <nikkola@kth.se>
2012-11-24 03:06:00 +0100

    Fixed TGA header again (accidently remove support for Visual Studio)

--- client/source/TGA.h
+++ client/source/TGA.h
@@ -21,23 +21,45 @@ enum class TGAEnc
     RunLengthEncRGB = 10
 };
 
+#ifdef _MSC_VER
+#pragma pack(push,1)
 struct TGAHeader
 {
-    i8  IDLength;
-    i8  ColorMapType;
-    i8  DataTypeCode;
+    i8 IDLength;
+    i8 ColorMapType;
+    i8 DataTypeCode;
 
     i16 ColorMapOrigin;
     i16 ColorMapLength;
-    i8  ColorMapDepth;
+    i8 ColorMapDepth;
 
     i16 XOrigin;
     i16 YOrigin;
     i16 Width;
     i16 Height;
-    i8  BPP;
-    i8  Descriptor;
+    i8 BPP;
+    i8 Descriptor;
+};
+#pragma pack(pop)
+#else
+struct TGAHeader
+{
+    i8 IDLength;
+    i8 ColorMapType;
+    i8 DataTypeCode;
+
+    i16 ColorMapOrigin;
+    i16 ColorMapLength;
+    i8 ColorMapDepth;
+
+    i16 XOrigin;
+    i16 YOrigin;
+    i16 Width;
+    i16 Height;
+    i8 BPP;
+    i8 Descriptor;
 } __attribute__ ((__packed__));
+#endif
 
 
 class TGA
