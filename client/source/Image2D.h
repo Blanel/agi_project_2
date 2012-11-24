a5f7a033646044452eb95e8d452de7c843c9ba7c
Erik N <nikkola@kth.se>
2012-11-24 02:07:37 +0100

    Seperated the TGA class to a seperate file + Additional fixes to pixel conversion

--- client/source/Image2D.h
+++ client/source/Image2D.h
@@ -30,38 +30,52 @@ enum class ImageFormat
 namespace pixel
 {
 
+//forward decl
+struct RGB_u8;
+struct RGBA_u8;
+struct Gray_u8;
+struct Gray_f32;
+
 struct RGB_u8
 {
-	RGB_u8(u8 r, u8 g, u8 b) : r(r), g(g), b(b) {}
+	RGB_u8();
+	RGB_u8(u8 r, u8 g, u8 b);
+	RGB_u8(const RGB_u8& c);
+	RGB_u8(const RGBA_u8& c);
+	RGB_u8(const Gray_u8& c);
+	RGB_u8(const Gray_f32& c);
 
-	static PixelFormat pixel_format() { return PixelFormat::RGB_U8; }
+	static PixelFormat pixel_format();
 
 	u8 r, g, b;
 };
 
 struct RGBA_u8
 {
-	RGBA_u8(u8 r, u8 g, u8 b, u8 a) : r(r), g(g), b(b), a(a) {}
+	RGBA_u8();
+	RGBA_u8(u8 r, u8 g, u8 b, u8 a);
+	RGBA_u8(const RGBA_u8& c);
+	RGBA_u8(const RGB_u8& c, u8 a = 255);
 
-	static PixelFormat pixel_format() { return PixelFormat::RGBA_U8; }
+	static PixelFormat pixel_format();
 
 	u8 r, g, b, a;
 };
 
 struct Gray_u8
 {
-	Gray_u8(u8 val) : val(val) {}
+	Gray_u8(u8 val = 0);
 
-	static PixelFormat pixel_format() { return PixelFormat::GRAY_U8; }
+	static PixelFormat pixel_format();
 
 	u8 val;
 };
 
 struct Gray_f32
 {
-	Gray_f32(f32 val) : val(val) {}
+	Gray_f32(f32 val = 0);
 
-	static PixelFormat pixel_format() { return PixelFormat::GRAY_F32; }
+	static PixelFormat pixel_format();
 
 	f32 val;
 };
@@ -85,6 +99,20 @@ public:
 		, m_PixelFormat(T::pixel_format())
 	{}
 
+	template <typename U>
+	Image2D(const Image2D<U>& img)
+		: m_Pixels(img.width() * img.height())
+		, m_Width(img.width())
+		, m_Height(img.height())
+		, m_PixelFormat(T::pixel_format())
+	{
+		/*
+		for (u32 i = 0; i < img.m_Pixels.size(); ++i)
+		{
+			m_Pixels[img.m_Pixels[i]];
+		}
+		*/
+	}
 
 	Image2D(u32 w, u32 h, const std::vector<T>& pixeldata)
 		: m_Pixels(pixeldata)
@@ -134,111 +162,6 @@ public:
 	const std::vector<T>& data() const
 	{
 		return m_Pixels;
-	}
-};
-
-class TGAException : public std::exception
-{
-
-    std::string m_Msg;
-public:
-    TGAException(const std::string& str)
-        : m_Msg(str)
-    {}
-};
-
-enum class TGAEnc
-{
-    RawIndexed = 1,
-    RawRGB = 2,
-    RunLengthEncIndexed = 9,
-    RunLengthEncRGB = 10
-};
-
-struct TGAHeader
-{
-    i8  IDLength;
-    i8  ColorMapType;
-    i8  DataTypeCode;
-
-    i16 ColorMapOrigin;
-    i16 ColorMapLength;
-    i8  ColorMapDepth;
-
-    i16 XOrigin;
-    i16 YOrigin;
-    i16 Width;
-    i16 Height;
-    i8  BPP;
-    i8  Descriptor;
-} __attribute__ ((__packed__));
-
-
-class TGA
-{
-	static void write(const std::string& filename, const TGAHeader& header, const i8* dataptr, u32 byte)
-	{
-        if (dataptr == nullptr)
-            throw TGAException("Invalid data source pointer");
-        
-        std::ofstream imagefile(filename.c_str(), std::ios::out | std::ios::binary);
-
-        imagefile.write(reinterpret_cast<const i8*>(&header), sizeof(TGAHeader));
-        imagefile.write(dataptr, byte);
-        imagefile.close();
-	}
-
-public:	
-	template <typename T>
-	static void write(const std::string& filename, const Image2D<T>& img)
-	{
-        TGAHeader header;
-        memset(&header, 0, sizeof(TGAHeader));
-
-        header.Width = img.width();
-        header.Height = img.height();
-        header.BPP = sizeof(T) * 8;
-
-        std::cout << sizeof(header) << std::endl;
-
-        bool flip_rb = false;
-
-        switch(img.pixel_format())
-        {
-    	case PixelFormat::RGB_U8:
-    		header.DataTypeCode = 0x2;
-    		header.Descriptor = 0x20;
-    		flip_rb = true;
-    		break;
-    	case PixelFormat::RGBA_U8:
-    		header.DataTypeCode = 0x2;
-    		header.Descriptor = 0x28;
-    		flip_rb = true;
-    		break;
-		case PixelFormat::GRAY_U8:
-			header.DataTypeCode = 0x3;
-			header.Descriptor |= (1 << 5);
-			break;
-        }
-
-		auto dataptr = reinterpret_cast<const i8*>(&(img.data()[0]));
-		std::vector<T> pixeldata;
-
-        if (flip_rb)
-        {
-	        pixeldata = img.data();
-
-	        for (auto& pixel : pixeldata)
-	        {
-	        	std::swap(pixel.r, pixel.b);
-	        }
-
-	        dataptr = reinterpret_cast<const i8*>(&(pixeldata.data()[0]));
-        }
-
-        u32 byte = img.width() * img.height() * sizeof(T);
-
-        write(filename.c_str(), header, dataptr, byte);
 	}
 };
 
