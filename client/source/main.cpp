5347e34106765e0f1a917d163c9549ccb0f01bbe
Erik N <nikkola@kth.se>
2012-11-24 16:44:47 +0100

    Main.cpp cleanup

--- client/source/main.cpp
+++ client/source/main.cpp
@@ -1,26 +1,11 @@
-#include <iostream>
-#include <vector>
-
 #include "Log.h"
-
 #include "RenderClient.h"
 
-using namespace std;
 using namespace revel;
-using namespace revel::renderer;
-
-#include "SimplexNoise.h"
 
 int main(int argc, char *argv[])
 {
 	R_LOG_INFO("Initializing");
-
-	f32 x = SimplexNoise::noise(1, 1);
-
-	R_LOG_INFO("NOISE: " << x);
-
-	SimplexNoise::debug();
-
 	RenderClient renderclient;
 	return renderclient.run();
 }
