401f522511a65304ea2ade3ff4a432b8dbbdc60d
Erik N <nikkola@kth.se>
2012-11-25 23:31:04 +0100

    Initial effect file

---
+++ client/source/Effect.h
@@ -0,0 +1,24 @@
+#include "renderer/ShaderProgram.h"
+
+namespace revel
+{
+namespace renderer
+{
+	
+class RenderPass
+{
+	std::shared_ptr<ShaderProgram> m_pShaderProgram;
+
+};
+
+class Effect
+{
+	class Technique
+	{
+
+	};
+	
+};
+
+}	
+}
