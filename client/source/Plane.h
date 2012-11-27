ddb86e023413d9ca46291efc1fa8aa833b436032
Erik N <nikkola@kth.se>
2012-11-27 02:33:06 +0100

    Initial Plane class for the renderer

---
+++ client/source/Plane.h
@@ -0,0 +1,50 @@
+#ifndef PLANE_H_
+#define PLANE_H_
+
+#include "math/Vector3.h"
+#include "renderer/RenderContext.h"
+#include "Camera.h"
+
+namespace revel
+{
+
+
+class Plane
+{
+	std::shared_ptr<renderer::VertexArray>		m_pVertexArray;
+	std::shared_ptr<renderer::ShaderProgram> 	m_pGpuProgram;
+	
+	point3 		m_Position;
+	//math::quat 	m_Orientation;
+
+	f32 m_Angle;
+
+	Plane(const std::shared_ptr<renderer::VertexArray>& va, std::shared_ptr<renderer::ShaderProgram>& sp, const point3& p = point3::(0, 0, 30), f32 angle = 0)	
+		: m_pVertexArray(va)
+		, m_pGpuProgram(sp)
+		, m_Position(p)
+		, m_Angle(angle)
+	{
+
+	}
+
+	virtual ~Plane()
+	{
+		
+	}
+
+	void draw(const std::shared_ptr<renderer::RenderContext>& ctx, const std::shared_ptr<Camera>& cam)
+	{
+		m_pVertexArray->bind();
+		m_pGpuProgram->use();
+
+
+
+		m_pVertexArray->unbind();
+	}
+};
+
+	
+}
+
+#endif // PLANE_H_
