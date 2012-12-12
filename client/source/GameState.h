f76a3c2f9480a10a37a2bfca995aafbbdff4317b
Eric Nellåker <eric.nellaker@gmail.com>
2012-12-12 01:13:44 +0100

    Basic gamestate interaction. Crashes unexpectant

--- client/source/GameState.h
+++ client/source/GameState.h
@@ -22,6 +22,14 @@ namespace revel
 
 		AirPlane();
 		~AirPlane();
+		
+		void set(f32 xn, f32 yn, f32 anglen, bool aliven)
+		{
+			x = xn;
+			y = yn;
+			angle = anglen;
+			alive = aliven;
+		}
 
 	};
 
@@ -31,10 +39,20 @@ namespace revel
 		f32 x; 
 		f32 y;
 		f32 angle;
+		bool alive;
 		bool hit;
 
 		Bullet();
 		~Bullet();
+		
+		void set(f32 xn, f32 yn, f32 anglen, bool aliven, bool hitn)
+		{
+			x = xn;
+			y = yn;
+			angle = anglen;
+			alive = aliven;
+			hit = hitn;
+		}
 	};
 
 
@@ -57,11 +75,29 @@ namespace revel
 			airplanes.insert(std::pair<i32,AirPlane>(id,p));
 		}
 		
-		void update_plane(i32 id, f32 x, f32 y, f32 angle)
+		void update_plane(i32 id, f32 x, f32 y, f32 angle, bool alive)
 		{
-		
+			airplanes.find(id)->second.set(x,y,angle,alive);
 		}
-
+		
+		void create_bullet(i32 id, f32 x, f32 y, f32 angle)
+		{
+			Bullet b;
+			b.x = x;
+			b.y = y;
+			b.angle = angle;
+			b.alive = true;
+			b.hit = false;
+			
+			bullets.insert(std::pair<i32,Bullet>(id,b));
+			
+		}
+		
+		void update_bullet(i32 id, f32 x, f32 y, f32 angle, bool alive, bool hit)
+		{
+			bullets.find(id)->second.set(x,y,angle,alive,hit);
+		}
+		
 		std::map<i32, AirPlane>& get_planes() 
 		{ 
 			return airplanes; 
