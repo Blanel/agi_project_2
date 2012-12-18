55594d490436288fcf0394625b9ec06aae343a6d
Erik N <nikkola@kth.se>
2012-12-18 04:22:47 +0100

    Testing FS effects

--- client/source/shaders/screen.fs
+++ client/source/shaders/screen.fs
@@ -6,5 +6,9 @@ out vec4 color;
 
 void main()
 {
-	color = texture2D(fbo_texture, uv0);
+	vec2 texcoord = uv0;
+
+	//texcoord.x += sin(texcoord.y *4*2*3.141592 + 3.0) / 100;
+
+	color = texture2D(fbo_texture, texcoord);
 }
