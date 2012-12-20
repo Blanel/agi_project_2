99e3c9f4abde46129825c848080cf19b6fd68159
Erik N <nikkola@kth.se>
2012-12-20 21:50:04 +0100

    Added cube generation

--- client/source/geo/Mesh.cpp
+++ client/source/geo/Mesh.cpp
@@ -69,32 +69,143 @@ std::shared_ptr<Mesh>
 Mesh::create_cube()
 {        
     auto mesh = std::make_shared<Mesh>();        
-    auto pos = mesh->create_vertex_attrib<point3>("position", 8);
+    auto pos = mesh->create_vertex_attrib<point3>("position");
+    auto normals = mesh->create_vertex_attrib<vec3>("normal");
+    auto uv = mesh->create_vertex_attrib<vec2>("texcoord");
 
+    //front
     pos->data().push_back(point3(-1, -1,  1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(0, 0, 1));
+
     pos->data().push_back(point3( 1, -1,  1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(0, 0, 1));
+
     pos->data().push_back(point3( 1,  1,  1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(0, 0, 1));
+    
     pos->data().push_back(point3(-1,  1,  1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(0, 0, 1));
+
+    //right
+    pos->data().push_back(point3( 1, -1,  1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(1, 0, 0));
+
+    pos->data().push_back(point3( 1, -1, -1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(1, 0, 0));
+
+    pos->data().push_back(point3( 1,  1, -1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(1, 0, 0));
+    
+    pos->data().push_back(point3( 1,  1, -1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(1, 0, 0));
+
+    //back
+    pos->data().push_back(point3( 1, -1, -1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(0, 0, -1));
 
     pos->data().push_back(point3(-1, -1, -1));
-    pos->data().push_back(point3( 1, -1, -1));
-    pos->data().push_back(point3( 1,  1, -1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(0, 0, -1));
+
     pos->data().push_back(point3(-1,  1, -1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(0, 0, -1));
+    
+    pos->data().push_back(point3( 1,  1, -1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(0, 0, -1));
+
+    //left
+    pos->data().push_back(point3(-1, -1, -1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(-1, 0, 0));
+
+    pos->data().push_back(point3(-1, -1,  1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(-1, 0, 0));
+
+    pos->data().push_back(point3(-1,  1,  1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(-1, 0, 0));
+    
+    pos->data().push_back(point3(-1,  1, -1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(-1, 0, 0));
+
+    //left
+    pos->data().push_back(point3(-1, -1, -1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(-1, 0, 0));
+
+    pos->data().push_back(point3(-1, -1,  1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(-1, 0, 0));
+
+    pos->data().push_back(point3(-1,  1,  1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(-1, 0, 0));
+    
+    pos->data().push_back(point3(-1,  1, -1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(-1, 0, 0));
+
+    //top
+    pos->data().push_back(point3(-1,  1,  1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(0, 1, 0));
+
+    pos->data().push_back(point3( 1,  1,  1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(0, 1, 0));
+
+    pos->data().push_back(point3( 1,  1, -1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(0, 1, 0));
+    
+    pos->data().push_back(point3(-1,  1, -1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(0, 1, 0));
+
+    //bottom
+    pos->data().push_back(point3(-1, -1, -1));
+    uv->data().push_back(vec2(0, 0));
+    normals->data().push_back(vec3(0, -1, 0));
+
+    pos->data().push_back(point3( 1, -1, -1));
+    uv->data().push_back(vec2(1, 0));
+    normals->data().push_back(vec3(0, -1, 0));
+
+    pos->data().push_back(point3( 1, -1, 1));
+    uv->data().push_back(vec2(1, 1));
+    normals->data().push_back(vec3(0, -1, 0));
+    
+    pos->data().push_back(point3(-1, -1, 1));
+    uv->data().push_back(vec2(0, 1));
+    normals->data().push_back(vec3(0, -1, 0));
 
     auto indices = mesh->indices<u32>();
 
     indices->add_triangle(0, 1, 2); //front
     indices->add_triangle(2, 3, 0);
-    indices->add_triangle(1, 5, 6); //right
-    indices->add_triangle(6, 2, 1);
-    indices->add_triangle(4, 0, 3); //left
-    indices->add_triangle(3, 7, 4);
-    indices->add_triangle(4, 5, 6); //back
+    indices->add_triangle(4, 5, 6); //right
     indices->add_triangle(6, 7, 4);
-    indices->add_triangle(3, 2, 6); //top
-    indices->add_triangle(6, 7, 3);
-    indices->add_triangle(4, 5, 1); //bottom
-    indices->add_triangle(1, 0, 4);
+    indices->add_triangle(8, 9, 10); //back
+    indices->add_triangle(10, 11, 8);
+    indices->add_triangle(12, 13, 14); //left
+    indices->add_triangle(14, 15, 12);
+    indices->add_triangle(16, 17, 18); //top
+    indices->add_triangle(18, 19, 16);
+    indices->add_triangle(20, 21, 22); //bottom
+    indices->add_triangle(22, 23, 20);
 
     mesh->add_vertex_attrib(pos);
     
