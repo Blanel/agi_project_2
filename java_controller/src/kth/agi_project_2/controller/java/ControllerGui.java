c5c481a6c97390f3bb066691ced9feb95ba182f1
Eric Nellåker <eric.nellaker@gmail.com>
2013-01-14 22:58:46 +0100

    Stubs for java controller added

---
+++ java_controller/src/kth/agi_project_2/controller/java/ControllerGui.java
@@ -0,0 +1,11 @@
+package kth.agi_project_2.controller.java;
+
+public class ControllerGui {
+	
+	private JavaController jc;
+	public ControllerGui(JavaController jc)
+	{
+		this.jc = jc;
+	}
+
+}
---
+++ java_controller/src/kth/agi_project_2/controller/java/JavaController.java
@@ -0,0 +1,23 @@
+package kth.agi_project_2.controller.java;
+
+import kth.agi_project_2.controller.AbstractController;
+
+public class JavaController extends AbstractController {
+
+	private ControllerGui cg;
+	public JavaController(ControllerGui cg)
+	{
+		super();
+		this.cg = cg;
+		
+	}
+	protected void lifeChangeEvent(int l) {
+		// TODO Auto-generated method stub
+
+	}
+	protected double getRotation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
